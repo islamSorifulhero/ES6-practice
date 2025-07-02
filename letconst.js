91669d808bba078909937dcb2b5374e5b96a98bb
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-02 20:26:18 +0600

    my 2nd commit

---
+++ letconst.js
@@ -0,0 +1,16 @@
+const myWife = "Dot Dot Rani";
+// myWife = "kichu Akta hobe";
+console.log(myWife);
+
+
+let myWife1 = "Dot Dot Rani";
+myWife1 = "kichu Akta hobe";
+console.log(myWife1);
+
+const numbers = [33, 55, 63, 99];
+numbers[2] = 163;
+console.log(numbers);
+
+const friendsNames = ['korim', 'rohim', 'roni', 'joni'];
+friendsNames [1] = 'Hujur rohim';
+console.log(friendsNames);
