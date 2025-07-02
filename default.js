25716868578a4d584ee9902255ff63464977798e
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-02 20:50:05 +0600

    my 3rd commit

---
+++ default.js
@@ -0,0 +1,29 @@
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+
+// const totalAmount = add(15, 25);
+// console.log(totalAmount);
+
+
+// function added(result1, result2){
+//     return result1 + result2;
+// }
+// const totalResult = added(20, 30);
+// console.log(totalResult);
+
+// function add(num1, num2){
+//     if (num2 == undefined){
+//         num2 = 0;
+//     }
+//     return num1 + num2;
+// }
+// const totalNum = add(11);
+// console.log(totalNum);
+
+function add(num1, num2){
+    num2 = num2 || 0;
+    return num1 + num2;
+}
+const total = add(35);
+console.log(total);
