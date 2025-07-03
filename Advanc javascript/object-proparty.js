0675052a50027adac9dffa3a94510bfa10364ba3
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-03 16:12:31 +0600

    map related practice

---
+++ Advanc javascript/object-proparty.js
@@ -0,0 +1,12 @@
+const students = [
+    {id: 21, name: 'Omar Sunny'},
+    {id: 31, name: 'Manna'},
+    {id: 41, name: 'Moyouri'},
+    {id: 71, name: 'Deepjol'}
+]
+
+// const names = students.map( s => s.name);
+// console.log(names);
+
+const ids = students.map(s => s.id);
+console.log(ids);
