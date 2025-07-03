5de3e5c40b57c2ab70b3c21eb7d307e5db17ad1f
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-03 15:24:00 +0600

    map, filter, find

---
+++ Advanc javascript/double vs triple-equal.js
@@ -0,0 +1,25 @@
+// const first = 2;
+// const second = 3;
+// if(first == second){
+//     console.log("condition is true")
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+//output condition is false
+
+// const first = 2;
+// const second = 2;
+// if(first == second){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+//output true
+
+
+
+
---
+++ Advanc javascript/map-filter.js
@@ -0,0 +1,30 @@
+const numbers = [3, 4, 5, 6, 7, 8];
+// const output = [];
+
+// for(let i = 0; i < numbers.length; i++){
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+// console.log(output);
+
+// function square (element){
+//     return element * element;
+// }
+
+// numbers.map(function(element){
+//     console.log(element);
+// })
+
+
+// const result = numbers.map(x => x * x);
+// console.log(result);
+
+// const bigger = numbers.filter(x => x > 5);
+// console.log(bigger);
+
+// const lower = numbers.filter(x => x < 5);
+// console.log(lower);
+
+const isthere = numbers.find(x => x > 6);
+console.log(isthere);
