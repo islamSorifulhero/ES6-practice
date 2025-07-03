ae51f2f7ffa2518703d211bb69d375592afc54a6
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-03 09:42:29 +0600

    null vs undefined

---
+++ Advanc javascript/null vs undefined.js
@@ -0,0 +1,10 @@
+// let pakhi ;
+// console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+const result = add(10, 23);
+console.log(result);
--- Advanc javascript/true-false.js
+++ Advanc javascript/true-false.js
@@ -1,7 +1,53 @@
-const age = 6;
-if(age > 0){
+// const age = 6;
+// if(age > 0){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false")
+// }
+
+// const age = 6;
+// if(age){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false")
+// }
+
+// const age = 0;
+// if(age){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false")
+// }
+
+
+// const age = "";
+// if(age){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false")
+// }
+
+const age = " ";
+if(age){
     console.log("condition is true");
 }
 else{
     console.log("condition is false")
 }
+
+//note
+
+//False gulo hobe
+//0
+//""
+//undefined
+//null
+//NaN
+
+//Truth gulo hobe
+//"0"
+//" "
