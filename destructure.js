5a6c228e32b4ba91f9cd1bb32b46b90398477e76
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-03 08:02:29 +0600

    my 5th commit

---
+++ destructure.js
@@ -0,0 +1,10 @@
+// const person = {name: 'Jack William', age: 23, job: 'facebook', gfName:'fulcoli', friendsName: ['jamal', 'kamal', 'totol', 'rohim']}
+// console.log(person.gfName);
+
+
+// const person = {name: 'Jack William', age: 23, job: 'facebook', gfName:'fulcoli', friendsName: ['jamal', 'kamal', 'totol', 'rohim']}
+// console.log(person.friendsName);
+
+
+const person = {name: 'Jack William', age: 23, job: 'facebook', gfName:'fulcoli', friendsName: ['jamal', 'kamal', 'totol', 'rohim']}
+console.log(person.friendsName[2]);
---
+++ inheritance.js
@@ -0,0 +1,9 @@
+class Child{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+const baby = new Child("Arnold");
+const baby2 = new Child("Tom");
+console.log(baby2);
