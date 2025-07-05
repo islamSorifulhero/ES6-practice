d24991d0fb54d0b6cb957b1b5909b57b708a24ac
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-05 19:50:57 +0600

    setTimeout and setInterval practice

---
+++ BIND-CALL-APPY/timeout.js
@@ -0,0 +1,39 @@
+// console.log(222222);
+// console.log(333333);
+// console.log(444444);
+
+
+// function secondPerson(){
+//     console.log(333333);
+// }
+// console.log(222222);
+// secondPerson();
+// console.log(4444444);
+
+
+// function secondPerson(){
+//     console.log(333333);
+// }
+// console.log(222222);
+// setTimeout(secondPerson);
+// console.log(4444444);
+// console.log(4444444);
+
+
+function secondPerson(){
+    console.log(333333);
+}
+console.log(222222);
+setTimeout(secondPerson, 3000);   //  3 second por run, 1000 a 1 second.
+console.log(4444444);
+
+
+// function secondPerson(){
+//     console.log(333333);
+// }
+// console.log(222222);
+// setTimeout(() => {                  //setInterval(function(){})
+//     console.log('See you later.')
+// },4000)
+// console.log(4444444);
+// console.log(8888888);
